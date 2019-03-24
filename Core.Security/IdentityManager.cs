10f320bbc9d4b55db0db72beb1174c6e9d08d258
Chris Doty <dotyc@civicinc.com>
2019-03-23 22:18:53 -0500

    refactor: move tcpip to CLIENTIP claim

--- Core.Security/IdentityManager.cs
+++ Core.Security/IdentityManager.cs
@@ -10,35 +10,34 @@ namespace Core.Security
 {
     public static class IdentityManager
     {
-        //public static string ClientMachine
-        //{
-        //    get
-        //    {
-        //        var client = "";
-        //        if (HttpContext.Current == null) client = Environment.MachineName;
-        //        else if (!string.IsNullOrEmpty(HttpContext.Current.Request.UserHostName))
-        //            client = HttpContext.Current.Request.UserHostName;
-        //        else if (!string.IsNullOrEmpty(HttpContext.Current.Request.UserHostAddress))
-        //            client = HttpContext.Current.Request.UserHostAddress;
+        public static string GetClientMachine(ClaimsPrincipal user)
+        {
+            //var client = "";
+            //if (HttpContext.Current == null) client = Environment.MachineName;
+            //else if (!string.IsNullOrEmpty(HttpContext.Current.Request.UserHostName))
+            //    client = HttpContext.Current.Request.UserHostName;
+            //else if (!string.IsNullOrEmpty(HttpContext.Current.Request.UserHostAddress))
+            //    client = HttpContext.Current.Request.UserHostAddress;
 
-        //        if (HttpContext.Current != null && IdentityConfig.Current.TransformXForwardedFor)
-        //        {
-        //            // x-forwarded-for check
-        //            foreach (var key in HttpContext.Current.Request.Headers.AllKeys)
-        //            {
-        //                if (key.ToLowerInvariant() == "x-forwarded-for")
-        //                {
-        //                    client = HttpContext.Current.Request.Headers[key];
-        //                    break;
-        //                }
-        //            }
-        //        }
+            //if (HttpContext.Current != null && IdentityConfig.Current.TransformXForwardedFor)
+            //{
+            //    // x-forwarded-for check
+            //    foreach (var key in HttpContext.Current.Request.Headers.AllKeys)
+            //    {
+            //        if (key.ToLowerInvariant() == "x-forwarded-for")
+            //        {
+            //            client = HttpContext.Current.Request.Headers[key];
+            //            break;
+            //        }
+            //    }
+            //}
 
-        //        if (client == "::1") client = Environment.MachineName;
-        //        if (string.IsNullOrEmpty(client)) client = "Unknown";
-        //        return client;
-        //    }
-        //}
+            var client = GetClaimValue(StandardClaimTypes.CLIENTIP);
+
+            if (client == "::1") client = Environment.MachineName;
+            if (string.IsNullOrEmpty(client)) client = "Unknown";
+            return client;
+        }
 
         public static string GetUsername(IPrincipal user)
         {

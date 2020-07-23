885d3fd85d80fc9fead36fe1faf840459ee192ce
Chris Doty <dotyc@civicinc.com>
2020-07-23 01:40:11 -0500

    fix: build

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.9.175")]
+[assembly: AssemblyFileVersion("2.0.9.0")]
 [assembly: AssemblyCompany("Chris Doty")]
 [assembly: AssemblyCopyright("Copyright © 2020 Chris Doty")]

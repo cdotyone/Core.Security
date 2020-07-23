caf12a5efe302426edd0a4e7d8067cc80bcac8da
Chris Doty <dotyc@civicinc.com>
2020-07-23 12:25:06 -0500

    chore: cleanup assembly metadata

--- Core.Security/Properties/AssemblyInfo.cs
+++ Core.Security/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Core.Security")]
+[assembly: AssemblyTitle("Codigo.Core.Security")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Codigo.Core")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: Guid("BADDFE41-8EE0-4679-BC94-D8AF51651088")]
